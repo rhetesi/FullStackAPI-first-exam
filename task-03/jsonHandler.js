bc21ecacf56a75a97bd6d1fd2f618d73cc417a66
rhetesi <rhetesi@gmail.com>
2021-07-05 15:56:12 +0200

    Task-03 works

--- task-03/jsonHandler.js
+++ task-03/jsonHandler.js
@@ -1,21 +1,28 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
+const fsp = require('fs').promises;
 
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
- const jsonPath = '';
+const {
+    join
+} = require('path');
 
- /**
-  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
-  * majd visszaadja a kapott tömböt.
-  * @returns objektumok tömbje
-  */
- const getList = async () => {
-     //
- };
+const jsonPath = join(__dirname, 'db', 'products.json');
+
+/**
+ * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
+ * majd visszaadja a kapott tömböt.
+ * @returns objektumok tömbje
+ */
+const getList = async () => {
+    //
+    const content = await fsp.readFile(jsonPath, 'utf8');
+    return JSON.parse(content);
+};
 
 /**
  * 4. A kapott tömböt json formátumra alakítja és beleírja a jsonPath útvonalon 
@@ -25,6 +32,8 @@
  */
 const saveList = async (list = []) => {
     //
+    await fsp.writeFile(jsonPath, JSON.stringify(list, null, 4), 'utf8');
+    return true;
 };
 
 /**
@@ -36,6 +45,17 @@ const saveList = async (list = []) => {
  */
 const update = async (entity = {}) => {
     //
+    const list = await getList();
+
+    const index = list.findIndex(item => item.id === entity.id);
+    list[index] = {
+        ...list[index],
+        ...entity
+    };
+
+    await saveList(list);
+
+    return list[index];
 };
 
 module.exports = {
