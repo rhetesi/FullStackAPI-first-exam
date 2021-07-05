77d69e69d1e3ef89b678ebeda19a52e2ba2e8cbe
rhetesi <rhetesi@gmail.com>
2021-07-05 15:41:44 +0200

    Task-02 works fine

--- task-02/reader.js
+++ task-02/reader.js
@@ -1,12 +1,16 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promis alapú verzióját használd.
  */
+const fsp = require('fs').promises;
 
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
-const jsonPath = '';
+const {
+    join
+} = require('path');
+const jsonPath = join(__dirname, 'db', 'users.json');
 
 /**
  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
@@ -14,8 +18,10 @@ const jsonPath = '';
  * 
  * @returns objektumok tömbje
  */
- const getList = async () => {
-     //
+const getList = async () => {
+    //
+    const content = await fsp.readFile(jsonPath, 'utf8');
+    return JSON.parse(content);
 };
 
 /**
@@ -28,6 +34,8 @@ const jsonPath = '';
  */
 const get = async (id = 0) => {
     //
+    const list = await getList();
+    return list.find(item => item.id === id);
 };
 
 module.exports = {
